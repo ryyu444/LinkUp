61fa3a3341287af063967d734319f05708d1f023
Mingzhe Wu <wmz2002914@hotmail.com>
2025-06-05 23:23:23 -0700

    add button for back to dashboard

--- linkup/src/app/(pages)/_components/profile/profile.tsx
+++ linkup/src/app/(pages)/_components/profile/profile.tsx
@@ -1,4 +1,7 @@
+'use client';
 import ProfileForm from './profileForm/profileForm';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 /*
   Entire profile for Create/Edit Profile in Figma (with image, upload, & buttons) 
@@ -17,38 +20,57 @@ function MainPage() {
   );
 }
 
+
 function Header() {
+  const router = useRouter();
+
+  function goToDashboard() {
+    router.push('/dashboard');
+  }
+
   return (
     <div className="relative h-[318px] w-[100vw]">
       
       {/* Blue bar */}
       <div className="absolute top-[72px] w-[100vw] h-[154px] bg-[#002855]"></div> 
+      
       {/* Logo */}
       <div className="absolute w-[169px] h-[41px] top-[15px] left-[12px]">
-        <img src="logo.svg"
-        alt="logo" className="w-full h-full object-cover" />
+        <img src="logo.svg" alt="logo" className="w-full h-full object-cover" />
       </div>
+
       {/* Back to Dashboard button */}
-      <div className="absolute w-[250px] h-[54px] top-[122px] right-[222px] rounded-[10px] border-2 border-[#F5F5F5] bg-white">
+      <div
+        className="absolute w-[250px] h-[54px] top-[122px] right-[222px] rounded-[10px] border-2 border-[#F5F5F5] bg-white cursor-pointer"
+        onClick={goToDashboard}
+        role="button"
+        tabIndex={0}
+      >
         <div className="absolute w-[230px] h-[32px] top-[10px] left-[10px]">
-          <img src="BacktoDashboard.svg" alt="editprofile" className="w-full h-full object-cover" />
+          <img src="BacktoDashboard.svg" alt="back to dashboard" className="w-full h-full object-cover" />
         </div>
       </div>
+
       {/* Edit Profile button */}
       <div className="absolute w-[170px] h-[54px] top-[122px] right-[28px] rounded-[10px] border-2 border-white">
         <div className="absolute w-[149px] h-[32px] top-[10px] left-[10px]">
-          <img src="EditProfile.svg" alt="editprofile" className="w-full h-full object-cover" />
+          <img src="EditProfile.svg" alt="edit profile" className="w-full h-full object-cover" />
         </div>
       </div>
+
       {/* user's photo */}
       <div className="absolute top-[146px] left-[61px] w-[172px] h-[172px] rounded-full border-[5px] border-white overflow-hidden">
-        <img src="https://teachmeanatomy.info/wp-content/uploads/Head-Cover-Photo.png"
-        alt="head" className="w-full h-full object-cover" />
+        <img
+          src="https://teachmeanatomy.info/wp-content/uploads/Head-Cover-Photo.png"
+          alt="head"
+          className="w-full h-full object-cover"
+        />
       </div>
 
     </div>
   );
 }
+
 
 function NameSection() {
   return (
@@ -157,6 +179,10 @@ function StudyInterest() {
 
 
 export default function Profile() {
+  const router = useRouter();
+  function handleClick() {
+    router.push('/dashboard');  // navigates to /other-page
+  }
   return (
     <div>
       <MainPage />
