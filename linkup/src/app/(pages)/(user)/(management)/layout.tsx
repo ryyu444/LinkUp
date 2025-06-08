adb8dd487728d12c5ee4ca7d115a566942a80763
Ryan <ryyu444@gmail.com>
2025-06-08 00:31:31 -0700

    style changes

--- linkup/src/app/(pages)/(user)/(management)/layout.tsx
+++ linkup/src/app/(pages)/(user)/(management)/layout.tsx
@@ -8,27 +8,39 @@ export default function UserManagementLayout({
   children: React.ReactNode;
 }) {
   return (
-     <main className="min-h-screen flex items-center justify-center bg-blue-50 px-4">
-      <div className="flex w-full max-w-5xl bg-white rounded-lg shadow-md overflow-hidden">
+    <main className='h-screen w-screen bg-blue-50'>
+      <div className='flex w-full h-full bg-white shadow-md overflow-hidden'>
         {/* Left Panel */}
         {/* logo */}
-        <div className="hidden md:flex flex-col justify-between bg-blue-950 text-white p-8 w-1/2">
-          <div className="mb-8">
-            <Image src="/signin_logo.png" alt="Logo" width={80} height={80} />
+        <div className='hidden md:flex flex-col justify-between bg-blue-950 text-white p-8 w-1/2'>
+          <Image src='/signin_logo.png' alt='Logo' width={80} height={80} />
+          <div className='flex flex-col items-center gap-6'>
+            <div className='flex flex-col items-center w-[80%] h-[45rem] relative overflow-hidden'>
+              <Image
+                src='/library.png'
+                alt='Library'
+                fill
+                style={{ objectFit: 'cover' }}
+                sizes='(max-width: 768px) 100vw, 50vw'
+                priority
+              />
+            </div>
           </div>
           {/* image */}
-          <div className="text-center flex flex-col items-center">
-            <Image src="/library.png" alt="Library" width={3000} height={300} className="mb-4" />
+          <div className='text-center flex flex-col items-center'>
             {/* text */}
-            <h2 className="text-xl font-semibold text-center">Find your perfect study partners</h2>
-            <p className="text-sm mt-2 text-center">
-              Connect with fellow students and improve your academic performance.
+            <h2 className='text-xl font-semibold text-center'>
+              Find your perfect study partners
+            </h2>
+            <p className='text-sm mt-2 text-center'>
+              Connect with fellow students and improve your academic
+              performance.
             </p>
           </div>
         </div>
 
         {/* Right Panel (Form) */}
-        <div className="w-full md:w-1/2 p-8">
+        <div className='w-1/2 flex flex-col justify-center items-center p-8'>
           {children}
         </div>
       </div>
