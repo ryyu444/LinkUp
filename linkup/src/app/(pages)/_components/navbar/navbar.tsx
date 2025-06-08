79524a3f62c2e2fb6d927c48019b43f612e6e348
Ryan <ryyu444@gmail.com>
2025-06-07 23:58:57 -0700

    made more changes + added task comments

--- linkup/src/app/(pages)/_components/navbar/navbar.tsx
+++ linkup/src/app/(pages)/_components/navbar/navbar.tsx
@@ -1,17 +1,23 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext, useState } from 'react';
+import { AuthContext } from '@/app/(pages)/_contexts/AuthContext';
 import { Button } from '@/app/(pages)/_components/ui/button';
 
 /*
-  Render login/signup button only on home page
-  Render small circle with user picture if they are logged in
-    - Clicking on the circle redirects to /user
+  TODO
+   1. Need to render the login and signup buttons only if the user is not logged in
+   2. If the user is logged in, render a profile icon that opens a dropdown menu with profile, my sessions, and logout
+
 */
 export default function Navbar() {
+  const auth = useContext(AuthContext);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
     // space between logo and buttons with padding and bottom border
-    <header className='flex justify-between items-center p-6 border-b'>
+    <header className='h-[64px] flex justify-between items-center p-6 border-b'>
       {/* the logo */}
       <div className='flex items-center space-x-2'>
         <Image src='/logo.png' alt='LinkUp Logo' width={32} height={32} />
